198cba2fd4c064c0d23c106fcd5c49a561de3847
Nidhi <nidhi.shah2793@gmail.com>
2020-11-19 19:01:57 -0500

    made cookie secure

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        // secure: true,
+        secure: true,
         expires: Date.now() + 900000, //in ms
         maxAge: 900000
     }
